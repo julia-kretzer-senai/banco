2ec85a039acccab8f53c2756e4b36900372cfc78
julia-kretzer-senai <109150681+julia-kretzer-senai@users.noreply.github.com>
2022-09-13 12:21:24 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -4,11 +4,12 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Pessoa {
-	Scanner Sc = new Scanner(System.in);
+	static Scanner Sc = new Scanner(System.in);
 
 	public int num;
 	public int yes;
 	public int confirm;
+	public String nome, endereco, prof, cpf, renda;
 
 	public void cadastrar() {
 
@@ -18,69 +19,59 @@ public class Pessoa {
 		int digito = random.nextInt(9);
 
 		System.out.println("Digite seu nome: ");
-		String nome = Sc.next();
+		nome = Sc.nextLine();
 
-		System.out.println("Digite seu endere�o: ");
-		String endereco = Sc.nextLine();
-
+		System.out.println("Digite seu endereco: ");
 		endereco = Sc.nextLine();
 
 		System.out.println("Digite sua profissão: ");
-		String prof = Sc.next();
+		prof = Sc.nextLine();
 
 		System.out.println("Digite seu CPF: ");
-		String cpf = Sc.next();
+		cpf = Sc.nextLine();
 
 		System.out.println("Digite sua renda: ");
-		String renda = Sc.next();
+		renda = Sc.nextLine();
 
 		System.out.println(
 				"Escolha um tipo de conta: \n1 - MERCURY\n Conta padrão, sem taxas \n2- VENUS\n Taxa de inscrição: R$100,00\n Recebe 2% de cashback todo mês para a realização de investimentos conosco \n3 - EARTH (RECOMENDADO) \n Taxa de adesão: R$300,00 \n10% de cashback mensal para a realização de investimentos em criptomoedas ");
 		int conta = Sc.nextInt();
 
-		String sConfirm = "";
-
 		if (conta == 1) {
 			System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: " + cpf
 					+ "\nRenda: " + renda + "\nConta MERCURY\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-			sConfirm = Sc.next();
-
-			confirm = Integer.valueOf(sConfirm);
+			confirm = Sc.nextInt();
 		} else if (conta == 2) {
 			System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: " + cpf
 					+ "\nRenda: " + renda + "\nConta VENUS\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-			sConfirm = Sc.next();
-
-			confirm = Integer.valueOf(sConfirm);
+			confirm = Sc.nextInt();
 		} else {
 			System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: " + cpf
 					+ "\nRenda: " + renda + "\nConta EARTH\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-			sConfirm = Sc.next();
-
-			confirm = Integer.valueOf(sConfirm);
+			confirm = Sc.nextInt();
 		}
 
+		
 		while (confirm != 1) {
 
 			System.out.println(
 					"Para mudar o nome: digite 1\nPara mudar o endereco: digite 2\nPara mudar a profissao: digite 3 \nPara mudar o CPF: digite 4 \nPara mudar a renda: digite 5\nPara mudar o tipo da conta: digite 6\nPara voltar: digite 7");
-			String sChange = Sc.next();
-
-			int change = Integer.valueOf(sChange);
+			int change = Sc.nextInt();
 
 			if (change == 1) {
 				System.out.println("Insira o nome correto: ");
-				nome = Sc.next();
+				Sc.nextLine();
+				nome = Sc.nextLine();
 				System.out.println("Nome modificado com sucesso!");
 				confirm = 1;
 			} else if (change == 2) {
 				System.out.println("Insira o endereco correto: ");
-				endereco = Sc.next();
+				endereco = Sc.nextLine();
 				System.out.println("Endereco modificado com sucesso!");
 				confirm = 1;
 			} else if (change == 3) {
 				System.out.println("Insira a profissao correta: ");
-				prof = Sc.next();
+				prof = Sc.nextLine();
 				System.out.println("Profissao modificada com sucesso!");
 				confirm = 1;
 			} else if (change == 4) {
@@ -90,7 +81,7 @@ public class Pessoa {
 				confirm = 1;
 			} else if (change == 5) {
 				System.out.println("Insira a renda correta: ");
-				renda = Sc.next();
+				renda = Sc.nextLine();
 				System.out.println("Renda modificada com sucesso!");
 				confirm = 1;
 			} else if (change == 6) {
@@ -104,30 +95,24 @@ public class Pessoa {
 				if (conta == 1) {
 					System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
 							+ cpf + "\nRenda: " + renda + "\nConta MERCURY\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-					sConfirm = Sc.next();
+					confirm = Sc.nextInt();
 
-					confirm = Integer.valueOf(sConfirm);
 				} else if (conta == 2) {
 					System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
 							+ cpf + "\nRenda: " + renda + "\nConta VENUS\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-					sConfirm = Sc.next();
-
-					confirm = Integer.valueOf(sConfirm);
+					confirm = Sc.nextInt();
 				} else {
 					System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
 							+ cpf + "\nRenda: " + renda + "\nConta EARTH\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-					sConfirm = Sc.next();
-
-					confirm = Integer.valueOf(sConfirm);
+					confirm = Sc.nextInt();
 				}
 			}
 		}
-
 		if (confirm == 1) {
 			System.out.println("Crie uma senha de 6 digitos numericos (sem comecar com 0): ");
-			String sSenha = Sc.next();
-
-			int senha = Integer.valueOf(sSenha);
+			Sc.nextLine();
+			
+			int senha = Sc.nextInt();
 
 			while (senha < 100000 || senha > 999999) {
 				System.out.println("Senha invalida. Digite uma nova senha: ");
@@ -135,27 +120,75 @@ public class Pessoa {
 			}
 			System.out.println("Conta criada com sucesso! \nNumero: " + numeroDaConta + "\nAgencia: " + agencia
 					+ "\nDigito: " + digito + "\n\nDigite 1 para ir para a tela de login");
-			String sYes = Sc.next();
-
-			yes = Integer.valueOf(sYes);
+			num = Sc.nextInt();
 		}
+		
 	}
 
 	public void fazerLogin() {
-		Scanner Sc = new Scanner(System.in);
 
 		System.out.println("Nome: ");
-		String nomeLogin = Sc.next();
+		String nomeLogin = Sc.nextLine();
 
 		System.out.println("Senha: ");
-		String senhaLogin = Sc.next();
+		String senhaLogin = Sc.nextLine();
 
 		System.out.println("Login realizado com sucesso! Para ir para a tela inicial, digite 1");
-		String sNum = Sc.next();
-
+		String sNum = Sc.nextLine();
+		
 		num = Integer.valueOf(sNum);
 
-		Sc.close();
 	}
 
+	public void mostrarDados() {
+		System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
+				+ cpf + "\nRenda: " + renda + "\nConta MERCURY\nDESEJA MODIFICAR DADOS? 1-SIM 2-NAO");
+		String sChange = Sc.nextLine();
+		int change = Integer.valueOf(sChange);
+		
+		if (change == 2) {
+			confirm = 1;
+		} else {
+			System.out.println(
+					"Para mudar o nome: digite 1\nPara mudar o endereco: digite 2\nPara mudar a profissao: digite 3 \nPara mudar o CPF: digite 4 \nPara mudar a renda: digite 5\nPara mudar o tipo da conta: digite 6\nPara voltar: digite 7");
+			String sMod = Sc.nextLine();
+
+			int mod = Integer.valueOf(sChange);
+
+			if (change == 1) {
+				System.out.println("Insira o nome correto: ");
+				nome = Sc.nextLine();
+				System.out.println("Nome modificado com sucesso!");
+				confirm = 1;
+			} else if (change == 2) {
+				System.out.println("Insira o endereco correto: ");
+				endereco = Sc.nextLine();
+				System.out.println("Endereco modificado com sucesso!");
+				confirm = 1;
+			} else if (change == 3) {
+				System.out.println("Insira a profissao correta: ");
+				prof = Sc.nextLine();
+				System.out.println("Profissao modificada com sucesso!");
+				confirm = 1;
+			} else if (change == 4) {
+				System.out.println("Insira o CPF correto: ");
+				cpf = Sc.nextLine();
+				System.out.println("CPF modificado com sucesso!");
+				confirm = 1;
+			} else if (change == 5) {
+				System.out.println("Insira a renda correta: ");
+				renda = Sc.nextLine();
+				System.out.println("Renda modificada com sucesso!");
+				confirm = 1;
+			} else if (change == 6) {
+				System.out.println("Escolha um tipo de conta: \n1 - MERCURY\n Conta padrão, sem taxas \n2- VENUS\n Taxa de inscrição: R$100,00"
+						+ "\n Recebe 2% de cashback todo mês para a realização de investimentos conosco \n3 - EARTH (RECOMENDADO)"
+						+ "\n Taxa de adesão: R$300,00 \n10% de cashback mensal para a realização de investimentos em criptomoedas");
+				int conta = Sc.nextInt();
+				System.out.println("Tipo da conta modificado com sucesso!");
+				confirm = 1;
+		}
+	}
+}
+	
 }
