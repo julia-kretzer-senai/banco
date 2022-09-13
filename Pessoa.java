8d5b16c1f37aacaaadb7e10f042ded2619a70140
julia-kretzer-senai <109150681+julia-kretzer-senai@users.noreply.github.com>
2022-09-13 09:45:31 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -4,12 +4,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Pessoa {
+	Scanner Sc = new Scanner(System.in);
+
 	public int num;
 	public int yes;
 	public int confirm;
 
 	public void cadastrar() {
-		Scanner Sc = new Scanner(System.in);
 
 		Random random = new Random();
 		int numeroDaConta = random.nextInt(999999999) + 100000000;
@@ -17,47 +18,69 @@ public class Pessoa {
 		int digito = random.nextInt(9);
 
 		System.out.println("Digite seu nome: ");
-		String nome = Sc.nextLine();
+		String nome = Sc.next();
 
 		System.out.println("Digite seu endere�o: ");
 		String endereco = Sc.nextLine();
 
+		endereco = Sc.nextLine();
+
 		System.out.println("Digite sua profissão: ");
-		String prof = Sc.nextLine();
+		String prof = Sc.next();
 
 		System.out.println("Digite seu CPF: ");
-		String cpf = Sc.nextLine();
+		String cpf = Sc.next();
 
 		System.out.println("Digite sua renda: ");
-		String renda = Sc.nextLine();
+		String renda = Sc.next();
 
-		System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "CPF: " + cpf
-				+ "Renda: " + renda + "\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-		String sConfirm = Sc.nextLine();
+		System.out.println(
+				"Escolha um tipo de conta: \n1 - MERCURY\n Conta padrão, sem taxas \n2- VENUS\n Taxa de inscrição: R$100,00\n Recebe 2% de cashback todo mês para a realização de investimentos conosco \n3 - EARTH (RECOMENDADO) \n Taxa de adesão: R$300,00 \n10% de cashback mensal para a realização de investimentos em criptomoedas ");
+		int conta = Sc.nextInt();
 
-		confirm = Integer.valueOf(sConfirm);
+		String sConfirm = "";
+
+		if (conta == 1) {
+			System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: " + cpf
+					+ "\nRenda: " + renda + "\nConta MERCURY\nCONFIRMAR DADOS? 1-SIM 2-NAO");
+			sConfirm = Sc.next();
+
+			confirm = Integer.valueOf(sConfirm);
+		} else if (conta == 2) {
+			System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: " + cpf
+					+ "\nRenda: " + renda + "\nConta VENUS\nCONFIRMAR DADOS? 1-SIM 2-NAO");
+			sConfirm = Sc.next();
+
+			confirm = Integer.valueOf(sConfirm);
+		} else {
+			System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: " + cpf
+					+ "\nRenda: " + renda + "\nConta EARTH\nCONFIRMAR DADOS? 1-SIM 2-NAO");
+			sConfirm = Sc.next();
+
+			confirm = Integer.valueOf(sConfirm);
+		}
 
 		while (confirm != 1) {
 
 			System.out.println(
-					"Para mudar o nome: digite 1\nPara mudar o endereco: digite 2\nPara mudar a profissao: digite 3 \nPara mudar o CPF: digite 4 \nPara mudar a renda: digite 5\nPara voltar: digite 6");
-			String sChange = Sc.nextLine();
+					"Para mudar o nome: digite 1\nPara mudar o endereco: digite 2\nPara mudar a profissao: digite 3 \nPara mudar o CPF: digite 4 \nPara mudar a renda: digite 5\nPara mudar o tipo da conta: digite 6\nPara voltar: digite 7");
+			String sChange = Sc.next();
 
 			int change = Integer.valueOf(sChange);
 
 			if (change == 1) {
 				System.out.println("Insira o nome correto: ");
-				nome = Sc.nextLine();
+				nome = Sc.next();
 				System.out.println("Nome modificado com sucesso!");
 				confirm = 1;
 			} else if (change == 2) {
 				System.out.println("Insira o endereco correto: ");
-				endereco = Sc.nextLine();
+				endereco = Sc.next();
 				System.out.println("Endereco modificado com sucesso!");
 				confirm = 1;
 			} else if (change == 3) {
 				System.out.println("Insira a profissao correta: ");
-				prof = Sc.nextLine();
+				prof = Sc.next();
 				System.out.println("Profissao modificada com sucesso!");
 				confirm = 1;
 			} else if (change == 4) {
@@ -66,22 +89,43 @@ public class Pessoa {
 				System.out.println("CPF modificado com sucesso!");
 				confirm = 1;
 			} else if (change == 5) {
-				System.out.println("Insira a renda correto: ");
-				renda = Sc.nextLine();
+				System.out.println("Insira a renda correta: ");
+				renda = Sc.next();
 				System.out.println("Renda modificada com sucesso!");
 				confirm = 1;
+			} else if (change == 6) {
+				System.out.println("Escolha um tipo de conta: \n1 - MERCURY\n Conta padrão, sem taxas \n2- VENUS\n Taxa de inscrição: R$100,00"
+						+ "\n Recebe 2% de cashback todo mês para a realização de investimentos conosco \n3 - EARTH (RECOMENDADO)"
+						+ "\n Taxa de adesão: R$300,00 \n10% de cashback mensal para a realização de investimentos em criptomoedas");
+				conta = Sc.nextInt();
+				System.out.println("Tipo da conta modificado com sucesso!");
+				confirm = 1;
 			} else {
-				System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "CPF: " + cpf
-				+ "Renda: " + renda + "\nCONFIRMAR DADOS? 1-SIM 2-NAO");
-				sConfirm = Sc.nextLine();
-				confirm = Integer.valueOf(sConfirm);
+				if (conta == 1) {
+					System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
+							+ cpf + "\nRenda: " + renda + "\nConta MERCURY\nCONFIRMAR DADOS? 1-SIM 2-NAO");
+					sConfirm = Sc.next();
 
+					confirm = Integer.valueOf(sConfirm);
+				} else if (conta == 2) {
+					System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
+							+ cpf + "\nRenda: " + renda + "\nConta VENUS\nCONFIRMAR DADOS? 1-SIM 2-NAO");
+					sConfirm = Sc.next();
+
+					confirm = Integer.valueOf(sConfirm);
+				} else {
+					System.out.println("Nome: " + nome + "\nEndereco: " + endereco + "\nProfissao:" + prof + "\nCPF: "
+							+ cpf + "\nRenda: " + renda + "\nConta EARTH\nCONFIRMAR DADOS? 1-SIM 2-NAO");
+					sConfirm = Sc.next();
+
+					confirm = Integer.valueOf(sConfirm);
+				}
 			}
 		}
 
 		if (confirm == 1) {
 			System.out.println("Crie uma senha de 6 digitos numericos (sem comecar com 0): ");
-			String sSenha = Sc.nextLine();
+			String sSenha = Sc.next();
 
 			int senha = Integer.valueOf(sSenha);
 
@@ -91,7 +135,7 @@ public class Pessoa {
 			}
 			System.out.println("Conta criada com sucesso! \nNumero: " + numeroDaConta + "\nAgencia: " + agencia
 					+ "\nDigito: " + digito + "\n\nDigite 1 para ir para a tela de login");
-			String sYes = Sc.nextLine();
+			String sYes = Sc.next();
 
 			yes = Integer.valueOf(sYes);
 		}
@@ -101,16 +145,16 @@ public class Pessoa {
 		Scanner Sc = new Scanner(System.in);
 
 		System.out.println("Nome: ");
-		String nomeLogin = Sc.nextLine();
+		String nomeLogin = Sc.next();
 
 		System.out.println("Senha: ");
-		String senhaLogin = Sc.nextLine();
+		String senhaLogin = Sc.next();
 
 		System.out.println("Login realizado com sucesso! Para ir para a tela inicial, digite 1");
-		String sNum = Sc.nextLine();
+		String sNum = Sc.next();
 
 		num = Integer.valueOf(sNum);
-		
+
 		Sc.close();
 	}
 
