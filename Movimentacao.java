6c3e36dc4dc53f8a9ef9e6ee946e4ced5b93b90b
julia-kretzer-senai <109150681+julia-kretzer-senai@users.noreply.github.com>
2022-09-13 12:20:53 -0300

    Update Movimentacao.java

--- Movimentacao.java
+++ Movimentacao.java
@@ -1,5 +1,5 @@
 package mine;
-
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Movimentacao {
@@ -17,7 +17,7 @@ public class Movimentacao {
 	public void depositar() {
 		System.out.print("Insira o valor do deposito: ");
 		deposito = Sc.nextDouble();
-		saldo = saldo + deposito;
+		saldo += deposito;
 
 		System.out.println("\nDepósito realizado com sucesso! Seu saldo atual é de R$" + saldo);
 		System.out.println("\nPara encerrar a sessão, digite 1\nPara continuar, digite 2");
@@ -29,10 +29,10 @@ public class Movimentacao {
 		saque = Sc.nextDouble();
 
 		while (saque > saldo) {
-			System.out.print("Você não possui esse saldo em conta. Insira outro valor: ");
+			System.out.print("Você não possui esse saldo em conta. \nInsira 1 para ir para a tela de depósito \nou \nInsira outro valor (maior que 1): ");
 			saque = Sc.nextDouble();
 		}
-		saldo = saldo - saque;
+		saldo -= saque;
 		System.out.println("\nSaque realizado com sucesso! Seu saldo atual é de R$" + saldo);
 		System.out.println("\nPara encerrar a sessão, digite 1\nPara continuar, digite 2");
 		continuar = Sc.nextInt();
@@ -48,7 +48,7 @@ public class Movimentacao {
 			transferencia = Sc.nextDouble();
 
 			while (transferencia > saldo) {
-				System.out.print("Você não possui esse saldo em conta. Insira outro valor: ");
+				System.out.print("Você não possui esse saldo em conta. \nInsira 1 para ir para a tela de depósito \nou \nInsira outro valor (maior que 1): ");
 				transferencia = Sc.nextDouble();
 			}
 
@@ -67,22 +67,73 @@ public class Movimentacao {
 	}
 	
 	public void verPortfolio() {
-		double btc = 0;
-		double eth = 0;
-		double doge = 0;
-		System.out.println("CRIPTOPORTFÓLIO\nBitcoin: " + btc + "\nEthereum: " + eth + "\nDogecoin: " + doge);
-		System.out.println("\n\n1- COMPRAR MOEDAS\n2- SAIR");
-		String sBuy = Sc.nextLine();
 		
-		int buy = Integer.valueOf(sBuy);
+		int back = 0;
+		double bitcoin = 111174;
+		double ethereum = 8370;
+		double dogecoin = 0.32;
 		
-		if (buy == 2) {
-			continuar = 2;
-		} else {
-			System.out.println("Qual moeda deseja comprar?");
+		DecimalFormat df = new DecimalFormat("#####.#####"); 
+		
+		while (back != 2) {
+			// reais / precobtc = quantidadebitcoin
+			// quantidadebtc * precobtc = reais
+			double btcSaldo = 0;
+			double ethSaldo = 0;
+			double dogeSaldo = 0;
+			double btc = btcSaldo * bitcoin;
+			double eth = ethSaldo * ethereum;
+			double doge = dogeSaldo * dogecoin;
+			
+			System.out.println("CRIPTO PORTFÓLIO\nBitcoin: " + btcSaldo + " BTC = R$" + btc + "\nEthereum: " + ethSaldo + " ETH = R$" + eth + "\nDogecoin: " + dogeSaldo + " DOGE = R$" + doge);
+			System.out.println("\n1- COMPRAR MOEDAS\n2- VOLTAR");
+			
+			int buy = Sc.nextInt();
+			int i = 0;
+			double qtd;
+			
+			if (buy == 2) {
+				back = 2;
+				continuar = 2;
+			} else {
+				System.out.println("Qual moeda deseja comprar? \n1- Bitcoin \n2- Ethereum \n3- Dogecoin");
+				Sc.nextLine();
+				int coin = Sc.nextInt();
+				
+				System.out.println("Quantos R$ deseja usar para comprar?");
+//				Sc.nextLine();
+				qtd = Sc.nextDouble();
+				
+				while (qtd > saldo) {
+					System.out.print("Você não possui esse saldo em conta. \nInsira outro valor: ");
+					qtd = Sc.nextDouble();
+
+				if (qtd <= saldo) {
+					if (coin == 1) {
+						System.out.println("Compra de BTC realizada com sucesso! Digite 1 para voltar para o portfólio");
+						btcSaldo += qtd/bitcoin;
+						btc += qtd;
+						back = Sc.nextInt();
+					} else if (coin == 2) {
+						ethSaldo += qtd/bitcoin;
+						eth += qtd;
+						System.out.println("Compra de ETH realizada com sucesso! Digite 1 para voltar para o portfólio");
+						back = Sc.nextInt();
+					} else {
+						dogeSaldo += qtd/bitcoin;
+						doge += qtd;
+						System.out.println("Compra de DOGE realizada com sucesso! Digite 1 para voltar para o portfólio");
+						back = Sc.nextInt();
+					}
+					saldo -= qtd;
+				}			
+				
+				
+			}
 		}
+		
 	}
 	
 	
-	
+	}
 }
